13e4c7423ed1af689effedf79c456e199662604b
Danial <danny00b@gmail.com>
2021-04-14 11:31:44 -0700

    Adding in 1-9

--- main.js
+++ main.js
@@ -6,3 +6,43 @@ function one(){
     var choice = document.getElementById('xo1');
     choice.innerHTML = "X";
 };
+
+function two(){
+    var choice = document.getElementById('xo2');
+    choice.innerHTML = "X";
+};
+
+function three(){
+    var choice = document.getElementById('xo3');
+    choice.innerHTML = "X";
+};
+
+function four(){
+    var choice = document.getElementById('xo4');
+    choice.innerHTML = "X";
+};
+
+function five(){
+    var choice = document.getElementById('xo5');
+    choice.innerHTML = "X";
+};
+
+function six(){
+    var choice = document.getElementById('xo6');
+    choice.innerHTML = "X";
+};
+
+function seven(){
+    var choice = document.getElementById('xo7');
+    choice.innerHTML = "X";
+};
+
+function eight(){
+    var choice = document.getElementById('xo8');
+    choice.innerHTML = "X";
+};
+
+function nine(){
+    var choice = document.getElementById('xo9');
+    choice.innerHTML = "X";
+};
