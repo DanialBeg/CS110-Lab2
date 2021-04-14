4114447495d87a0892ba5ac66b97df6d5cf71049
Danial <danny00b@gmail.com>
2021-04-14 11:05:08 -0700

    pushing

--- main.js
+++ main.js
@@ -3,6 +3,6 @@ const y = 'Y';
 const counter = 0;
 
 function one(){
-    var choice = document.getElementsByClassName('xo');
+    var choice = document.getElementsByClassName('xo1');
     choice.innerHTML = "X";
 };
